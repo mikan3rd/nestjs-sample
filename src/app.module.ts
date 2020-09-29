89a5e382976dd5d8e49cf7547f2c4c59a80a3f77
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-09-29 23:40:24 +0900

    chore: 不要なcontroller削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModel } from './user/user.model';
 import { UserModule } from './user/user.module';
@@ -24,7 +23,6 @@ import { UserModule } from './user/user.module';
     }),
     UserModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/user/user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserModel } from './user.model';
-
-describe('User', () => {
-  it('should be defined', () => {
-    expect(new UserModel()).toBeDefined();
-  });
-});
