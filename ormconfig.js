8788ee133ad4b674cbfdf24b58be0bb4e3fdfddc
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-11-18 20:58:37 +0900

    fix: double quote

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,13 @@
 module.exports = {
-  type: 'mysql',
-  host: 'localhost',
+  type: "mysql",
+  host: "localhost",
   port: 3306,
-  username: 'root',
-  password: '',
-  database: 'nestjs-sample',
-  entities: ['src/**/*.model.ts'],
-  migrations: ['src/migrations/*.ts'],
+  username: "root",
+  password: "",
+  database: "nestjs-sample",
+  entities: ["src/**/*.model.ts"],
+  migrations: ["src/migrations/*.ts"],
   cli: {
-    migrationsDir: 'src/migrations',
+    migrationsDir: "src/migrations",
   },
 };
